04ad8fb18b3ce48045149045925e2ed0e65b6587
Matheus Guimarães <matheusguimaraes668@gmail.com>
2023-04-30 19:20:51 -0300

    Alterado o menu responsivo

--- script/navbar.js
+++ script/navbar.js
@@ -1,12 +1,18 @@
 export default function navBar() {
     const menu = document.querySelector('.menu-mobile');
-    
-    
+    const menuList = document.querySelector('#menu-mobile');
+    const quemSomos = document.querySelector('.li3-mobile');
 
     const showMenu = () => {
-        const menuList = document.querySelector('#menu-mobile');
         menuList.classList.toggle('show');
     }
 
-    menu.addEventListener('click' , showMenu);
+    menu.addEventListener('click', showMenu);
+
+    const notMenu = () => {
+        menuList.classList.remove('show');
+        console.log('ola')
+    }
+
+    quemSomos.addEventListener('click', notMenu);
 }
