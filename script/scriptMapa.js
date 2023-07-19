73386ed8b6541769ce233825c0e718addabbd264
Matheus Guimarães <matheusguimaraes668@gmail.com>
2023-07-19 20:17:23 -0300

    Trocada a localização do mapa

--- script/scriptMapa.js
+++ script/scriptMapa.js
@@ -1,11 +1,13 @@
-export default function initMapa(){
-    var map = L.map('map').setView([-22.11651052179692, -46.68643656043614], 20);
+export default function initMapa() {
+  var map = L.map('map').setView([-22.116826, -46.684173], 20);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-    
-    L.marker([-22.11651052179692, -46.68643656043614]).addTo(map)
-        .bindPopup('RDM Química está localizada aqui')
-        .openPopup();
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker([-22.116826, -46.684173])
+    .addTo(map)
+    .bindPopup('RDM Química está localizada aqui')
+    .openPopup();
 }
